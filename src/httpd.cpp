ef8cb845908deb5deaaf8b84959a570b3cee0a3b
Paul-Louis Ageneau <paul-louis@ageneau.org>
2012-08-03 18:05:06 +0200

    Added Httpd

---
+++ src/httpd.cpp
@@ -0,0 +1,133 @@
+/*************************************************************************
+ *   Copyright (C) 2011-2012 by Paul-Louis Ageneau                       *
+ *   paul-louis (at) ageneau (dot) org                                   *
+ *                                                                       *
+ *   This file is part of Arcanet.                                       *
+ *                                                                       *
+ *   Arcanet is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Affero General Public License as      *
+ *   published by the Free Software Foundation, either version 3 of      *
+ *   the License, or (at your option) any later version.                 *
+ *                                                                       *
+ *   Arcanet is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ *   GNU Affero General Public License for more details.                 *
+ *                                                                       *
+ *   You should have received a copy of the GNU Affero General Public    *
+ *   License along with Arcanet.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                         *
+ *************************************************************************/
+
+#include "httpd.h"
+
+namespace arc
+{
+
+Httpd::Httpd(int port) :
+		mSock(port)
+{
+
+}
+
+Httpd::~Httpd(void)
+{
+	mSock.close();
+	// TODO: delete clients
+}
+
+void Httpd::run(void)
+{
+	try {
+		while(true)
+		{
+			Socket sock = mSock.accept();
+			Handler *client = new Handler(this, sock);
+			client->start();
+		}
+	}
+	catch(const NetException &e)
+	{
+		return;
+	}
+}
+
+Httpd::Handler::Handler(Httpd *httpd, const Socket &sock) :
+		mHttpd(httpd),
+		mSock(sock)
+{
+
+}
+
+Httpd::Handler::~Handler(void)
+{
+	mSock.close();
+}
+
+void Httpd::Handler::run(void)
+{
+	String action;
+	if(!mSock.readLine(action)) return;
+	action.trim();
+	String url = action.cut(' ').trim();
+	String protocol = url.cut(' ').trim();
+
+	if(protocol != "GET" && protocol != "POST")
+		error(500);
+
+	if(url.empty() || protocol.empty())
+		error(500);
+
+	StringMap headers;
+	while(true)
+	{
+		String line;
+		assertIO(mSock.readLine(line));
+		if(line.empty()) break;
+
+		String value = line.cut(':');
+		line.trim();
+		value.trim();
+		headers.insert(line,value);
+	}
+
+	String file(url);
+	String getData = file.cut('?');
+	StringMap get;
+	if(!getData.empty())
+	{
+		std::list<String> exploded;
+		getData.explode(exploded,'&');
+		for(	std::list<String>::iterator it = exploded.begin();
+				it != exploded.end();
+				++it)
+		{
+			String value = it->cut('=');
+			get.insert(*it, value);
+		}
+	}
+
+	StringMap post;
+	if(protocol == "POST")
+	{
+		// TODO: read post data
+	}
+
+	process(file, url, headers, get, post);
+}
+
+void Httpd::Handler::error(int code)
+{
+	// TODO
+}
+
+void process(	const String &file,
+				const String &url,
+				StringMap &headers,
+				StringMap &get,
+				StringMap &post)
+{
+	// TODO
+}
+
+}
---
+++ src/httpd.h
@@ -0,0 +1,69 @@
+/*************************************************************************
+ *   Copyright (C) 2011-2012 by Paul-Louis Ageneau                       *
+ *   paul-louis (at) ageneau (dot) org                                   *
+ *                                                                       *
+ *   This file is part of Arcanet.                                       *
+ *                                                                       *
+ *   Arcanet is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Affero General Public License as      *
+ *   published by the Free Software Foundation, either version 3 of      *
+ *   the License, or (at your option) any later version.                 *
+ *                                                                       *
+ *   Arcanet is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ *   GNU Affero General Public License for more details.                 *
+ *                                                                       *
+ *   You should have received a copy of the GNU Affero General Public    *
+ *   License along with Arcanet.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                         *
+ *************************************************************************/
+
+#ifndef ARC_HTTPD_H
+#define ARC_HTTPD_H
+
+#include "include.h"
+#include "exception.h"
+#include "socket.h"
+#include "serversocket.h"
+#include "thread.h"
+
+namespace arc
+{
+
+class Httpd : public Thread
+{
+public:
+	Httpd(int port = 80);
+	~Httpd(void);
+
+private:
+	class Handler : public Thread
+	{
+	public:
+		Handler(Httpd *httpd, const Socket &sock);
+		~Handler(void);
+
+	private:
+		void run(void);
+		void error(int code);
+		void process(	const String &file,
+						const String &url,
+						StringMap &headers,
+						StringMap &get,
+						StringMap &post);
+
+		Httpd *mHttpd;
+		Socket mSock;
+	};
+
+	void run(void);
+
+	ServerSocket mSock;
+
+	friend class Handler;
+};
+
+}
+
+#endif
