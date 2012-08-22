bc1b67a6a0fca7ae2f6472498dfe501763a7fb67
Paul-Louis Ageneau <paul-louis@ageneau.org>
2012-08-23 01:01:34 +0200

    Added StripedFile

---
+++ src/stripedfile.cpp
@@ -0,0 +1,103 @@
+/*************************************************************************
+ *   Copyright (C) 2011-2012 by Paul-Louis Ageneau                       *
+ *   paul-louis (at) ageneau (dot) org                                   *
+ *                                                                       *
+ *   This file is part of Arcanet.                                       *
+ *                                                                       *
+ *   Arcanet is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Affero General Public License as      *
+ *   published by the Free Software Foundation, either version 3 of      *
+ *   the License, or (at your option) any later version.                 *
+ *                                                                       *
+ *   Arcanet is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ *   GNU Affero General Public License for more details.                 *
+ *                                                                       *
+ *   You should have received a copy of the GNU Affero General Public    *
+ *   License along with Arcanet.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                         *
+ *************************************************************************/
+
+#include "stripedfile.h"
+
+namespace arc
+{
+
+StripedFile::StripedFile(File *file, size_t blockSize, int nbStripes, int stripe) :
+		mFile(file),
+		mBlockSize(blockSize),
+		mStripeSize(blockSize/nbStripes),
+		mStripe(stripe)
+{
+	mFile->seekg(stripe*mStripeSize, File::beg);
+	mBlock = 0;
+	mOffset = 0;
+}
+
+StripedFile::~StripedFile(void)
+{
+
+}
+
+void StripedFile::seek(uint64_t position)
+{
+	seek(position / mStripeSize, position % mStripeSize);
+}
+
+void StripedFile::seek(size_t block, size_t offset)
+{
+	if(mBlock <= block)
+	{
+		mFile->seekg(offset-mOffset, File::cur);
+		mOffset = 0;
+	}
+	else {
+		mFile->seekg(mStripe*mStripeSize + offset, File::beg);
+		mBlock = 0;
+		mOffset = 0;
+	}
+
+	while(mBlock < block)
+	{
+		mFile->seekg(mBlockSize, File::cur);
+		++mBlock;
+	}
+}
+
+size_t StripedFile::readData(char *buffer, size_t size)
+{
+	if(!size) return 0;
+
+	const size_t left = std::min(mStripeSize - mOffset, size);
+	const size_t len = mFile->readData(buffer, left);
+	mOffset+= len;
+
+	if(mOffset == mStripeSize)
+	{
+		// Move to the beginning of the stripe on the next block
+		seek(mBlock+1, 0);
+		return len + readData(buffer+len, size-len);
+	}
+
+	return len;
+}
+
+void StripedFile::writeData(const char *buffer, size_t size)
+{
+	if(!size) return;
+
+	const size_t len = std::min(mStripeSize - mOffset, size);
+
+	mFile->writeData(buffer, len);
+	mOffset+= len;
+
+	if(mOffset == mStripeSize)
+	{
+		// Move to the beginning of the stripe on the next block
+		seek(mBlock+1, 0);
+		writeData(buffer+len, size-len);
+	}
+}
+
+}
---
+++ src/stripedfile.h
@@ -0,0 +1,55 @@
+/*************************************************************************
+ *   Copyright (C) 2011-2012 by Paul-Louis Ageneau                       *
+ *   paul-louis (at) ageneau (dot) org                                   *
+ *                                                                       *
+ *   This file is part of Arcanet.                                       *
+ *                                                                       *
+ *   Arcanet is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Affero General Public License as      *
+ *   published by the Free Software Foundation, either version 3 of      *
+ *   the License, or (at your option) any later version.                 *
+ *                                                                       *
+ *   Arcanet is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ *   GNU Affero General Public License for more details.                 *
+ *                                                                       *
+ *   You should have received a copy of the GNU Affero General Public    *
+ *   License along with Arcanet.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                         *
+ *************************************************************************/
+
+#ifndef ARC_STRIPEDFILE_H
+#define ARC_STRIPEDFILE_H
+
+#include "include.h"
+#include "file.h"
+
+namespace arc
+{
+
+class StripedFile : public ByteStream
+{
+public:
+	StripedFile(File *file, size_t blockSize, int nbStripes, int stripe);
+	~StripedFile(void);
+
+	void seek(uint64_t position);
+	void seek(size_t block, size_t offset);
+
+	size_t readData(char *buffer, size_t size);
+	void writeData(const char *buffer, size_t size);
+
+private:
+	File *mFile;
+	size_t mBlockSize;
+	size_t mStripeSize;
+	int mStripe;
+
+	size_t mBlock;		// Current block
+	size_t mOffset;		// Current position inside the current stripe
+};
+
+}
+
+#endif
