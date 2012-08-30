773926dea98581089367f1807b415158ada058e7
Paul-Louis Ageneau <paul-louis@ageneau.org>
2012-08-30 15:53:27 +0200

    Added Interface and Tracker

---
+++ src/tracker.h
@@ -0,0 +1,43 @@
+/*************************************************************************
+ *   Copyright (C) 2011-2012 by Paul-Louis Ageneau                       *
+ *   paul-louis (at) ageneau (dot) org                                   *
+ *                                                                       *
+ *   This file is part of Arcanet.                                       *
+ *                                                                       *
+ *   Arcanet is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Affero General Public License as      *
+ *   published by the Free Software Foundation, either version 3 of      *
+ *   the License, or (at your option) any later version.                 *
+ *                                                                       *
+ *   Arcanet is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ *   GNU Affero General Public License for more details.                 *
+ *                                                                       *
+ *   You should have received a copy of the GNU Affero General Public    *
+ *   License along with Arcanet.                                         *
+ *   If not, see <http://www.gnu.org/licenses/>.                         *
+ *************************************************************************/
+
+#ifndef ARC_TRACKER_H
+#define ARC_TRACKER_H
+
+#include "include.h"
+#include "http.h"
+
+namespace arc
+{
+
+class Tracker : public Http::Server
+{
+public:
+	Tracker(int port = 8080);
+	~Tracker();
+
+protected:
+	void process(Http::Request &request);
+};
+
+}
+
+#endif
