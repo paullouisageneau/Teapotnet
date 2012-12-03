6012397a3bbed7c144e4a420e7ad05a586827fa3
Paul-Louis Ageneau <paul-louis@ageneau.org>
2012-12-03 20:11:06 +0100

    Fixed missing CONST64

--- src/include.h
+++ src/include.h
@@ -24,7 +24,7 @@
 
 #define DEBUG
 #define APPNAME		"TeapotNet"
-#define APPVERSION	"0.3"
+#define APPVERSION	"0.3.1"
 #define APPAUTHOR	"Paul-Louis Ageneau"
 #define APPLINK		"http://www.teapotnet.org/"
 #define SOURCELINK	"http://www.teapotnet.org/source/"
