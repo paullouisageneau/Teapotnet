982f132a97526f0577881da1828d7fa137242b96
Paul-Louis Ageneau <paul-louis@ageneau.org>
2013-02-07 19:53:47 +0100

    Daemon install support for Debian

--- daemon.sh
+++ daemon.sh
@@ -7,45 +7,56 @@ fi
 
 TPROOT="/var/lib/teapotnet"
 
+install_all()
+{
+	groupadd teapotnet
+	useradd -g teapotnet -d $TPROOT -s /bin/bash teapotnet
+        mkdir -p $TPROOT
+	chown -R teapotnet.teapotnet $TPROOT
+	chmod 750 $TPROOT
+}
+
+uninstall_all()
+{
+	rm -rf $TPROOT
+        userdel teapotnet
+}
+
 if which systemctl &> /dev/null; then
 	case "$1" in
 	"install")
-		groupadd teapotnet
-		useradd -g teapotnet -d $TPROOT -s /bin/bash teapotnet
-		mkdir -p $TPROOT
-		chown -R teapotnet.teapotnet $TPROOT
-		chmod 750 $TPROOT
-	
-		cat > /etc/systemd/system/teapotnet.service << EOF
-[Unit]
-Description=TeapotNet
-After=local-fs.target network.target
-
-[Service]
-Type=simple
-User=teapotnet
-Group=teapotnet
-WorkingDirectory=/var/lib/teapotnet
-ExecStart=$PREFIX/bin/teapotnet
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
+		install_all
+		sed "s/\/usr\/bin\/teapotnet/$(echo $PREFIX | sed -e 's/\//\\\//g')\/bin\/teapotnet/g" teapotnet.service > /etc/systemd/system/teapotnet.service
 		echo "Run \"systemctl start teapotnet.service\" to start the daemon and go to http://localhost:8080/"
 		echo "Run \"systemctl enable teapotnet.service\" to start it at each boot"
 	;;
 	"uninstall")
 		rm -rf /etc/systemd/system/teapotnet.service
-		rm -rf $TPROOT
-		userdel teapotnet
+		uninstall_all
 	;;
 	*)
 		echo "Unknown operation. Usage: $0 (install|uninstall) [prefix]"
 	esac
+elif [ -d /etc/init.d ]; then
+	case "$1" in
+        "install")
+		install_all
+                sed "s/\/usr\/bin\/teapotnet/$(echo $PREFIX | sed -e 's/\//\\\//g')\/bin\/teapotnet/g" teapotnet.init > /etc/init.d/teapotnet
+                chmod +x /etc/init.d/teapotnet
+		update-rc.d teapotnet defaults
+		echo "Run \"/etc/init.d/teapotnet start\" to start the daemon and go to http://localhost:8080/"
+        ;;
+        "uninstall")
+		update-rc.d -f teapotnet remove
+                rm -rf /etc/init.d/teapotnet
+        	uninstall_all
+	;;
+        *)
+                echo "Unknown operation. Usage: $0 (install|uninstall) [prefix]"
+        esac
 else
 	if [ "$1" == "install" ]; then
-		echo "This system does not use systemd, you should set up teapotnet to start at boot time by yourself."
+		echo "This system does not use systemd or init.d, you should set up teapotnet to start at boot time by yourself."
 		echo "Launch teapotnet in the appropriate directory then go to http://localhost:8080/"
 	fi
 fi
