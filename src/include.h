c07dabec67b457b1e281d232adccdf26d8e89a52
Paul-Louis Ageneau <paul-louis@ageneau.org>
2013-03-06 23:18:00 +0100

    Version 0.4.4

--- src/include.h
+++ src/include.h
@@ -24,7 +24,7 @@
 
 #define DEBUG
 #define APPNAME		"TeapotNet"
-#define APPVERSION	"0.4.3"
+#define APPVERSION	"0.4.4"
 #define APPAUTHOR	"Paul-Louis Ageneau"
 #define APPLINK		"http://www.teapotnet.org/"
 #define SOURCELINK	"http://www.teapotnet.org/source/"
