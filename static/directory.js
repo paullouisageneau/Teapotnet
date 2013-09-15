3cc6ea8acd8d1aafe1c512e2d1e39c962e053eb9
haroine <antoine.rebecq@m4x.org>
2013-09-15 19:12:50 +0200

    Display modifications to directory list function

--- static/directory.js
+++ static/directory.js
@@ -19,7 +19,16 @@
  *   If not, see <http://www.gnu.org/licenses/>.                         *
  *************************************************************************/
 
-function listDirectory(url, object, userName = '') {
+
+function listDirectory(url, object) {
+
+	userName = '';
+	listDirectory(url, object, userName); // Some browsers don't support syntax username='' in prototype
+
+}
+
+
+function listDirectory(url, object, userName) {
 
 	$.ajax({
 		url: url,
@@ -28,7 +37,15 @@ function listDirectory(url, object, userName = '') {
 	})
 	.done(function(data) {
 		if(data && data.length > 0) {
-			$(object).html('<table class="files"></table>');
+
+			urlFile = url.substring(0,url.length-6);
+			var parentButton, parentButtonLink, isPlayableDirectory;
+			(url.charAt(url.length-6) == '/') ? parentButtonLink = '..' : parentButtonLink = '.'; // url.length-6 because url finishes by "?json"
+
+			parentButton = '<span class="button"> '+data.length+' files</span> <a href="'+parentButtonLink+'" class="button"> <img src="/arrow_up.png" alt="Parent"> </a>';
+
+			$(object).html(parentButton);
+			$(object).append('<table class="files"></table>');
                 	var table = $(object).find('table');
 
 			for(var i=0; i<data.length; i++) {
@@ -41,10 +58,35 @@ function listDirectory(url, object, userName = '') {
 						+ (resource.type == "directory" ? '/' : ''));
 
 				var line = '<tr>';
-				line+= '<td><a href="'+link.escape()+'">'+resource.name.escape()+'</a></td>';
+
+				if(resource.type == "file") 
+				{
+					var isPlayable;
+					var extension = resource.name.escape().substring(resource.name.escape().lastIndexOf('.')+1,resource.name.escape().length);
+					isPlayable = isPlayableResource(resource.name.escape());
+					isPlayableDirectory |= isPlayable;
+
+					line+= '<td class="icon"> <img src="/file.png" alt="(file)"> </td>';
+					//line+= ''
+					isPlayable ? line+= '<td class="filename"> <span class="type">'+extension+'  </span><a href="'+link.escape()+'?play=1">'+resource.name.escape()+'</a></td>' : line+= '<td class="filename"><span class="type">'+extension+'  </span><a href="'+link.escape()+'">'+resource.name.escape()+'</a></td>' ;
+					line+= '<td class="actions"> <a href="'+link.escape()+"?download=1"+'"> <img src="/down.png" alt="(download)"> </a> </td>';
+					if(isPlayable)
+					{
+						line+= '<td class="actions"> <a href="'+link.escape()+"?play=1"+'"> <img src="/play.png" alt="(play)"> </a> </td>';
+					}
+				}
+				else
+				{
+					line += '<tr class="dir">';
+					line+= '<td class="icon"> <img src="/dir.png" alt="(directory)"> </td>';
+					line+= '<td><a href="'+link.escape()+'">'+resource.name.escape()+'</a></td>';
+				}
 				line+= '</tr>';
 				table.append(line);
+
 			}
+
+			//if(isPlayableDirectory) $(object).prepend('<a href="'+urlFile+'?playlist=1" class="button"> Play this directory </a>'); // TODO
 		}
 		else {
 			$(object).html('No files');
@@ -57,4 +99,21 @@ function listDirectory(url, object, userName = '') {
 	.fail(function(jqXHR, textStatus) {
 		$(object).html('Failed');
 	});
+}
+
+function isPlayableResource(fileName)
+{
+	return (isAudio(fileName) || isVideo(fileName));
+}
+
+function isAudio(fileName)
+{
+	var regexp = new RegExp("(mp3|ogg|ogga|flac|wav|ape|aac|m4a|mp4a|wma)$","i");
+	return regexp.test(fileName);
+}
+
+function isVideo(fileName)
+{
+	var regexp = new RegExp("(avi|mkv|ogv|ogx|ogm|wmv|asf|flv|mpg|mpeg|mp4|m4v|mov|3gp|3g2|divx|xvid|rm|rv)$","i");
+	return regexp.test(fileName);
 }
