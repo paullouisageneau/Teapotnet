8705b225b4c42ebe1f42227499e3fca80f4b4a26
Paul-Louis Ageneau <paul-louis@ageneau.org>
2013-10-30 17:51:46 +0100

    Fixed possible deadlock

--- tpn/splicer.cpp
+++ tpn/splicer.cpp
@@ -301,6 +301,7 @@ size_t Splicer::readData(char *buffer, size_t size)
 	
 	if(!mCacheEntry->finished())
 	{
+		Desynchronize(this);
 		Synchronize(mCacheEntry);
 		
 		if(!mCacheEntry->isDownloading())
@@ -313,7 +314,6 @@ size_t Splicer::readData(char *buffer, size_t size)
 		{
 			if(isStarted() || mCacheEntry->isBlockDownloading(block)) 
 			{
-				Desynchronize(this);
 				mCacheEntry->wait(milliseconds(100));
 			}
 			else {
