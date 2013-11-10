83d49de59f3acbb44a8014f29c63c683dd165280
Paul-Louis Ageneau <paul-louis@ageneau.org>
2013-11-10 23:00:30 +0100

    Version 0.6.4

--- tpn/include.h
+++ tpn/include.h
@@ -24,7 +24,7 @@
 
 #define DEBUG
 #define APPNAME		"TeapotNet"
-#define APPVERSION	"0.6.3"
+#define APPVERSION	"0.6.4"
 #define APPAUTHOR	"Paul-Louis Ageneau"
 #define APPLINK		"http://teapotnet.org/"
 #define SOURCELINK	"http://teapotnet.org/source"
