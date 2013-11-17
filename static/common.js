4ccc6c347f8476b0c7885babc39b598108d8d4ed
Paul-Louis Ageneau <paul-louis@ageneau.org>
2013-11-17 21:32:31 +0100

    Fix for correct date formatting

--- static/common.js
+++ static/common.js
@@ -89,7 +89,7 @@ if(!String.contains) {
 function formatTime(timestamp) {
 	var date = new Date(timestamp*1000);
 	//return date.toLocaleDateString() + " " + date.toLocaleTimeString();
-	return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+	return date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + date.getDate()).slice(-2) + ' ' + ("0" + date.getHours()).slice(-2) + ':' + ("0" + date.getMinutes()).slice(-2) + ':' + ("0" + date.getSeconds()).slice(-2);
 }
 
 function getBasePath(nbFolders) {
