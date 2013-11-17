66a68d459f665c6b09daa8a777aaf651a5454c51
Paul-Louis Ageneau <paul-louis@ageneau.org>
2013-11-18 00:32:58 +0100

    Reduced timeout for interface port check on Windows

--- tpn/main.cpp
+++ tpn/main.cpp
@@ -319,7 +319,7 @@ int main(int argc, char** argv)
 #ifdef WINDOWS
 		if(!InterfacePort) try {
 			int port = Config::Get("interface_port").toInt();
-			Socket sock(Address("127.0.0.1", port), 1.);
+			Socket sock(Address("127.0.0.1", port), 0.1);
 			InterfacePort = port;
 		}
 		catch(...) {}
