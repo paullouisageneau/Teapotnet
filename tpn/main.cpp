d9a57c7a4feb884cfe9cec08ef3eaf1728cabf03
Paul-Louis Ageneau <paul-louis@ageneau.org>
2013-12-14 00:03:26 +0100

    Fixed local connect timeout

--- tpn/main.cpp
+++ tpn/main.cpp
@@ -397,7 +397,7 @@ int main(int argc, char** argv)
 		
 		if(!InterfacePort) try {
 			int port = Config::Get("interface_port").toInt();
-			Socket sock(Address("127.0.0.1", port), 0.2);
+			Socket sock(Address("127.0.0.1", port), 0.1);
 			InterfacePort = port;
 		}
 		catch(...) {}
