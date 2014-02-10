7ef2c97ecb27e8b205c80fc23ea74ae45eef6f02
haroine <antoine.rebecq@m4x.org>
2014-02-10 20:48:17 +0100

    Added pictograms

--- static/common.js
+++ static/common.js
@@ -406,22 +406,24 @@ function setMessagesReceiverRec(url, object, next) {
 						
 						if(!message.passed)
 						{
-							$('#'+id+' .buttonsbar').append('<a href="#" class="button passlink">Pass</a>');
+							$('#'+id+' .buttonsbar').append('<a href="#" class="button passlink"><img height="15" src="/pass_blue.png"/></a>');
 							(function(id, stamp) {
 								$('#'+id+' .passlink').click(function() {
-									$.post("messages/", { stamp: stamp, action: "pass", token: TokenMessage })
-									.done(function(data) {
-										$('#'+id+' .passlink').replaceWith('<span class="button">Passed</span>');
-									});
-									return false;
+									if(confirm('Do you want to pass this message to your friends ?')) {
+										$.post("messages/", { stamp: stamp, action: "pass", token: TokenMessage })
+										.done(function(data) {
+											$('#'+id+' .passlink').replaceWith('<span class="button"><img height="15" src="/pass_black.png"/></span>');
+										});
+										return false;
+									}
 								});
 							})(id, message.stamp);
 						}
 						else {
-							$('#'+id+' .buttonsbar').append('<span class="button">Passed</span>');
+							$('#'+id+' .buttonsbar').append('<span class="button"><img height="15" src="/pass_black.png"/></span>');
 						}
 						
-						$('#'+id+' .buttonsbar').append('<a href="#" class="button replylink">Reply</a>');
+						$('#'+id+' .buttonsbar').append('<a href="#" class="button replylink"><img height="15" src="/reply_black.png"/></a>');
 						(function(idReply) {
 							$('#'+id+' .replylink').click(function() {
 								$('#'+idReply).toggle();
