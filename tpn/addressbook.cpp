0976c86030ff2eb8ecf7733de9fb91da08fce2e0
Paul-Louis Ageneau <paul-louis@ageneau.org>
2014-12-01 10:44:35 +0100

    Fixed invitation deletion

--- tpn/addressbook.cpp
+++ tpn/addressbook.cpp
@@ -1155,8 +1155,13 @@ bool AddressBook::Invitation::recv(const Identifier &peer, const Notification &n
 		
 		LogDebug("AddressBook::Invitation", "Deleting invitation: " + peering().toString());
 		for(int i=0; i<mAddressBook->mInvitations.size(); ++i)
+		{
 			if(mAddressBook->mInvitations[i].peering() == peering())
+			{
 				mAddressBook->mInvitations.erase(i);
+				break;
+			}
+		}
 		
 		SafeWriteFile file(mAddressBook->mFileName);
 		JsonSerializer serializer(&file);
