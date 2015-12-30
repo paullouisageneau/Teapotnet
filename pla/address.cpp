4b937dc59fb6d057afbe7cabbd17b71ae73edea4
Paul-Louis Ageneau <paul-louis@ageneau.org>
2015-12-30 02:55:05 +0100

    Fixed ai_socktype in Address::Resolve()

--- pla/address.cpp
+++ pla/address.cpp
@@ -34,7 +34,7 @@ bool Address::Resolve(const String &host, const String &service, List<Address> &
 	addrinfo aiHints;
 	std::memset(&aiHints, 0, sizeof(aiHints));
 	aiHints.ai_family = AF_UNSPEC;
-	aiHints.ai_socktype = 0;
+	aiHints.ai_socktype = SOCK_STREAM;
 	aiHints.ai_protocol = 0;
 	if(host.contains(':') || !host.containsLetters()) aiHints.ai_flags = 0;
 	else aiHints.ai_flags = AI_ADDRCONFIG;
