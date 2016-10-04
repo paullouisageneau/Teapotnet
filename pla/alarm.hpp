4d880946aab42ae4029753a8ed3c2270fa652e3d
Paul-Louis Ageneau <paul-louis@ageneau.org>
2016-10-04 17:50:40 +0200

    Added Alarm

---
+++ pla/alarm.hpp
@@ -0,0 +1,172 @@
+/*************************************************************************
+ *   Copyright (C) 2011-2016 by Paul-Louis Ageneau                       *
+ *   paul-louis (at) ageneau (dot) org                                   *
+ *                                                                       *
+ *   This file is part of Plateform.                                     *
+ *                                                                       *
+ *   Plateform is free software: you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Affero General Public License as      *
+ *   published by the Free Software Foundation, either version 3 of      *
+ *   the License, or (at your option) any later version.                 *
+ *                                                                       *
+ *   Plateform is distributed in the hope that it will be useful, but    *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ *   GNU Affero General Public License for more details.                 *
+ *                                                                       *
+ *   You should have received a copy of the GNU Affero General Public    *
+ *   License along with Plateform.                                       *
+ *   If not, see <http://www.gnu.org/licenses/>.                         *
+ *************************************************************************/
+
+#ifndef PLA_ALARM_H
+#define PLA_ALARM_H
+
+#include "pla/include.hpp"
+
+namespace pla 
+{
+
+class Alarm
+{
+public:
+	using clock = std::chrono::steady_clock;
+	typedef std::chrono::duration<double> duration;
+	typedef std::chrono::time_point<clock, duration> time_point;
+	
+	Alarm(void);
+	template<class F, class... Args> Alarm(F&& f, Args&&... args);
+	~Alarm(void);
+	
+	template<class F, class... Args>
+	auto set(F&& f, Args&&... args)
+		-> std::future<typename std::result_of<F(Args...)>::type>;
+	
+	template<class F, class... Args>
+	auto schedule(time_point time, F&& f, Args&&... args)
+		-> std::future<typename std::result_of<F(Args...)>::type>;
+	
+	void schedule(time_point time);
+	void cancel(void);
+	void join(void);
+	
+private:
+	std::mutex mutex;
+	std::condition_variable condition;
+	std::thread thread;
+	std::function<void()> function;
+	time_point time;
+	bool stop;
+};
+
+inline Alarm::Alarm(void) : time(time_point::min()), stop(false)
+{
+	this->thread = std::thread([this]()
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		while(true)
+		{
+			if(this->time == time_point::min())
+			{
+				if(this->stop) break;
+				this->condition.wait(lock);
+			}
+			else {
+				if(this->time > clock::now())
+				{
+					this->condition.wait_until(lock, this->time);
+				}
+				else {
+					this->time = time_point::min();
+					lock.unlock();
+					this->function();
+				}
+			}
+		}
+	});
+}
+
+template<class F, class... Args>
+Alarm::Alarm(F&& f, Args&&... args) : Alarm()
+{
+	set(std::forward<F>(f), std::forward<Args>(args)...);
+}
+
+inline Alarm::~Alarm(void)
+{
+	join();
+}
+
+template<class F, class... Args>
+auto Alarm::set(F&& f, Args&&... args)
+	-> std::future<typename std::result_of<F(Args...)>::type>
+{
+	using type = typename std::result_of<F(Args...)>::type;
+
+	auto task = std::make_shared<std::packaged_task<type()> >(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+	std::future<type> result = task->get_future();	
+
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		this->function = [task]() { (*task)(); };
+	}
+	
+	return result;
+}
+
+template<class F, class... Args>
+auto Alarm::schedule(time_point time, F&& f, Args&&... args) 
+	-> std::future<typename std::result_of<F(Args...)>::type>
+{
+	using type = typename std::result_of<F(Args...)>::type;
+
+	auto task = std::make_shared<std::packaged_task<type()> >(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+	std::future<type> result = task->get_future();	
+
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		if(this->stop) throw std::runtime_error("schedule on stopped Alarm");
+		this->function = [task]() { (*task)(); };
+		this->time = time;
+	}
+	
+	condition.notify_all();
+	return result;
+}
+
+
+inline void Alarm::schedule(time_point time)
+{
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		if(this->stop) throw std::runtime_error("reschedule on stopped Alarm");
+		this->time = time;
+	}
+	
+	condition.notify_all();
+}
+
+inline void Alarm::cancel(void)
+{
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		this->time = time_point::min();
+	}
+	
+	condition.notify_all();
+}
+
+inline void Alarm::join(void)
+{
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		this->stop = true;
+	}
+	
+	condition.notify_all();
+	thread.join();
+}
+
+}
+
+#endif
