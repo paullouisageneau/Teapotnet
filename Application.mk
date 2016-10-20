e483ef4286941d5c82caf35ebf5e306ef95c572c
Paul-Louis Ageneau <paul-louis@ageneau.org>
2016-10-21 01:17:46 +0200

    Enable only armeabi-v7a

--- Application.mk
+++ Application.mk
@@ -1,5 +1,6 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -std=c++11 -frtti -fexceptions
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi-v7a x86
 APP_PLATFORM := android-8
 APP_OPTIM := release
+
