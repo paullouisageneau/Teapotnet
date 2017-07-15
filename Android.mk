f4e88b166c6492e2e5019812c5b94565605a3630
Paul-Louis Ageneau <paul-louis@ageneau.org>
2017-07-15 12:33:26 +0200

    Fixed paths in Android.mk

--- Android.mk
+++ Android.mk
@@ -12,44 +12,46 @@ ifeq ($(TARGET_ARCH_ABI), x86)
 CERBERO_ABI:= android_x86
 endif
 
+CERBERO:= $(HOME)/cerbero/build/dist/$(CERBERO_ABI)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := gnutls
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libgnutls.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libgnutls.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := nettle
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libnettle.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libnettle.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := hogweed
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libhogweed.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libhogweed.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := gmp
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libgmp.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libgmp.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := tasn1
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libtasn1.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libtasn1.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := intl
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libintl.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libintl.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := iconv
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libiconv.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libiconv.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := z
-LOCAL_SRC_FILES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/lib/libz.a
+LOCAL_SRC_FILES := $(CERBERO)/lib/libz.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -61,7 +63,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := teapotnet
 LOCAL_CFLAGS    := -DHAVE_PTHREADS -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/pla/*.cpp) $(wildcard $(LOCAL_PATH)/tpn/*.cpp) include/sqlite3.c
-LOCAL_C_INCLUDES := $(HOME)/cerbero/dist/$(CERBERO_ABI)/include $(HOME)/src/argon2/include
+LOCAL_C_INCLUDES := $(CERBERO)/include $(HOME)/src/argon2/include
 LOCAL_LDLIBS    := -llog
 LOCAL_STATIC_LIBRARIES := gnutls nettle hogweed gmp tasn1 intl iconv z argon2
 include $(BUILD_SHARED_LIBRARY)
